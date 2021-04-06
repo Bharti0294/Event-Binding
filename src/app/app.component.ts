d66602cdeb0d886a523ca334bca44c519401fc98
Bharti0294 <bharti.nehul07@gmail.com>
2021-04-06 00:30:33 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EventBinding';
+
+  months = ["January", "Feburary", "March", "April",
+  "May", "June", "July", "August", "September",
+  "October", "November", "December"];
+isavailable = true;
+myClickFunction(event: any) {
+alert("Button is clicked");
+console.log(event);
 }
+changemonths(event: any) {
+console.log("Changed month from the Dropdown");
+console.log(event);
+}
+}
+
+
